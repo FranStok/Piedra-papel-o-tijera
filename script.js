a93b582c96bca4564c6a72c38151c7f7e3c05db6
Francico <franciscoastoreca@hotmail.com>
2022-02-24 19:21:35 -0300

    commit

--- script.js
+++ script.js
@@ -10,6 +10,11 @@ function ComputerPlay(){
 }
 function PlayRound(Player, AI){
     let PlayerIns=Player.toLowerCase();
+    const Element=document.createElement("p");
+    const Textos=document.querySelector(".Textos");
+    Element.textContent="Elecciones: "+PlayerIns+" "+AI;
+    Element.classList.add("Elecciones");
+    Textos.appendChild(Element);
     console.log(PlayerIns + " " + AI);
     switch(PlayerIns){
         case "rock":{
@@ -49,7 +54,7 @@ function Verifica(P){
         alert("Mal ingreso");
         return false;
     }
-}
+}/*
 function game(){
     let I,PCount=0,AICount=0,P;
     let Result;
@@ -76,6 +81,22 @@ function game(){
         else   
             console.log("Empate");
     }
-}
-
-game();
+}*/
+const buttons=document.querySelectorAll("button");
+buttons.forEach(element => {
+    element.addEventListener("click",function(e){
+        let Eleccion=document.querySelector(".Elecciones");
+        if(Eleccion!=null){
+            Eleccion.remove();
+        }
+        let Result=document.querySelector(".Results");
+        if(Result!=null){
+            Result.remove();
+        }
+        Result=document.createElement("p");
+        const Textos=document.querySelector(".Textos");
+        Result.textContent=PlayRound(element.className,ComputerPlay())
+        Result.classList.add("Results");
+        Textos.appendChild(Result);
+    });
+});
